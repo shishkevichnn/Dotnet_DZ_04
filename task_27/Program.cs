8fae9efe553678a0709b6ffebf4114f47f3e543a
natalia shishkevich <shel_12@mail.ru>
2022-08-30 00:37:48 +0300

    Задача 27: Напишите функцию и запустите ее, которая принимает на вход число и выдаёт сумму цифр в числе.

---
+++ task_27/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 27: Напишите функцию и запустите ее, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int GetSum(int a)
+{
+    int sum = 0;
+    while (a > 0)
+    {
+    sum = sum + a % 10;
+    a /= 10;
+    }
+    return sum;
+}
+Console.Write(GetSum(number));
