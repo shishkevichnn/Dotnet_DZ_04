39db013b3f7c0be24307201412d8495be57297e1
natalia shishkevich <shel_12@mail.ru>
2022-08-30 13:00:00 +0300

    Задача 29: Напишите программу, которая задаёт массив из N элементов, заполненных случайными числами из [a, b) и выводит их на экран.

---
+++ task_29/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из N элементов, заполненных случайными числами из [a, b) 
+и выводит их на экран.
+5, 0, 20 -> [1, 2, 5, 7, 19]
+3, 1, 35 -> [6, 1, 33]*/
+Console.Write("Введите количество элементов в массиве : ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("Введите диапозон случайных чисел из [a, b), число а: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите диапозон случайных чисел из [a, b), число b: ");
+int b = int.Parse(Console.ReadLine());
+
+void RandArray ()
+{
+    int[] arr = new int [N];
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(a,b);
+        //Console.Write("{0} ", arr[i]);
+    }
+    Console.WriteLine($"[{String.Join(", ", arr)}]");
+}
+RandArray();
